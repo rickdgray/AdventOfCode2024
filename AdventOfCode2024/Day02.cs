c0dd569ab96b5fb1472e238df646d6f39873cbd0
Rick Gray <rickdgray@outlook.com>
2024-12-05 13:25:20 -0600

    Day 2 Part 1

--- AdventOfCode2024/Day02.cs
+++ AdventOfCode2024/Day02.cs
@@ -4,7 +4,37 @@ namespace AdventOfCode2024
     {
         public long Part1(List<string> data)
         {
-            throw new NotImplementedException();
+            var safe = 0L;
+
+            foreach (var line in data)
+            {
+                var levels = line.Split(' ').Select(long.Parse).ToList() ?? [];
+
+                var diffs = new List<long>();
+                for (int i = 1; i < levels.Count; i++)
+                {
+                    diffs.Add(levels[i] - levels[i - 1]);
+                }
+
+                if (!diffs.All(d => d > 0) && !diffs.All(d => d < 0))
+                {
+                    continue;
+                }
+
+                if (diffs.Any(d => d == 0))
+                {
+                    continue;
+                }
+
+                if (diffs.Any(d => Math.Abs(d) > 3))
+                {
+                    continue;
+                }
+
+                safe++;
+            }
+
+            return safe;
         }
 
         public long Part2(List<string> data)
