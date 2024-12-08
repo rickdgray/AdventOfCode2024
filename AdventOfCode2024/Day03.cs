bcdac033b97e09d800da9ea592e8e6a4912c49e6
Rick Gray <rickdgray@outlook.com>
2024-12-08 15:43:06 -0600

    Day 3 Part 1

--- AdventOfCode2024/Day03.cs
+++ AdventOfCode2024/Day03.cs
@@ -1,15 +1,35 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode2024
 {
-    public class Day03 : IDay
+    public partial class Day03 : IDay
     {
         public long Part1(List<string> data)
         {
-            throw new NotImplementedException();
+            var sum = 0L;
+
+            foreach (var line in data)
+            {
+                var regex = MulFunctionRegex();
+                var matches = regex.Matches(line);
+                foreach (Match match in matches)
+                {
+                    var values = match.Value.Split(',');
+                    var a = int.Parse(values[0][4..]);
+                    var b = int.Parse(values[1][..^1]);
+                    sum += a * b;
+                }
+            }
+
+            return sum;
         }
 
         public long Part2(List<string> data)
         {
             throw new NotImplementedException();
         }
+
+        [GeneratedRegex(@"mul\(\d+,\d+\)")]
+        private static partial Regex MulFunctionRegex();
     }
 }
